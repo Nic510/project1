e9f941f0100f41f76a2c4384322ab0df24dc7544
Nic Boutte <nicboutte@gmail.com>
2014-12-18 17:12:48 -0800

    prepare db

--- db.js
+++ db.js
@@ -3,18 +3,20 @@ var pg	=	require('pg');
 // Set up database
 var db = {};
 db.config = {
- database: "project1db",
-port: 5432,
-host: "localhost"
+//  database: "project1db",
+// port: 5432,
+// host: "localhost"
 };
 db.connect = function(runAfterConnecting) {
-pg.connect(db.config, function(err, client, done){
-if (err) {
-console.error("OOOPS!!! SOMETHING WENT WRONG!", err);
-}
-runAfterConnecting(client);
-done();
-});
+  console.log(process.env.DATABASE_URL);
+
+  pg.connect(process.env.DATABASE_URL, function(err, client, done){
+    if (err) {
+      console.error("OOOPS!!! SOMETHING WENT WRONG!", err);
+    }
+    runAfterConnecting(client);
+    done();
+  });
 };
 db.query = function(statement, params, callback){
 db.connect(function(client){
