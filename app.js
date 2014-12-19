28269099f68455a0d5047c919998c1360a56214f
Nic Boutte <nicboutte@gmail.com>
2014-12-18 17:05:55 -0800

    Add Procfile

--- app.js
+++ app.js
@@ -24,61 +24,43 @@ secret: 'keyboard cat',
 resave: false,
 saveUninitialized: true
 }))
-//Users
-var users = [
-	{ id: 3, username: 'nic', password: 'hey', email: 'nic@ga.com' }
-];
 
-
-function findByName(name, fn) {
-	db.query("select * from users where name=$1", [name], function(err, res) {
-			console.log(null, res.rows);
-			fn(null, res.rows[0])
-	});
-}
-function findById(id, fn) {
-	var idx = id - 1;
-	if (users[idx]) {
-		fn(null, users[idx]);
-	} else {
-		fn(new Error('User ' + id + ' does not exist'));
-	}
-}
 //Serializer
 passport.serializeUser(function(user, done) {
 	done(null, user.id);
 });
 // Deserializer
 passport.deserializeUser(function(id, done) {
-	findById(id, function (err, user) {
-		done(err, user);
+	db.query('SELECT * FROM users WHERE id = $1', [id], function(err, dbRes) {
+		if (!err) {
+		done(err, dbRes.rows[0]);
+		}
 	});
 });
-
+// Local Strategy
 var localStrategy = new LocalStrategy(function(name, password, done) {
-	findByName(name, function(err, user) {
-		console.log("authenticating")
-		console.log(user)
+	db.query('SELECT * FROM users WHERE name = $1', [name], function(err, dbRes) {
+    var user = dbRes.rows[0];
+    
 		if (err) { return done(err); }
 
-		// if (!user) { 
-		// 	return done(null, false, { message: 'Unknown user ' +user }); 
-		// }
-		// if (user.password != password) { 
-		// 	return done(null, false, { message: 'Invalid password' }); 
-		// }
+		if (!user) { 
+			return done(null, false, { message: 'Unknown user ' +user }); 
+		}
+		if (user.password != password) { 
+			return done(null, false, { message: 'Invalid password' }); 
+		}
 		return done(null, user);
 		})
 	}
 )
 
-
 passport.use(localStrategy);
 app.use(passport.initialize());
 app.use(passport.session());
 
-// Set up routes
-// Good
+////////////////////////////////////////////////////////////
+// app.get Routes
 app.get('/', function(req, res)	{
 		db.query('SELECT * FROM images;', function(err, dbRes) {
 		if(!err) {
@@ -86,15 +68,19 @@ app.get('/', function(req, res)	{
 		}
 	});
 });
-// Good
-app.get('/login', function(req, res) {
-	res.render('login');
+//Login Page
+app.get('/users/login', function(req, res) {
+	res.render('users/login');
 });
-//NOT Good
+// Posts Page
 app.get('/posts', function(req, res)	{
 	db.query('SELECT * FROM images;', function(err, dbRes) {
+		if(req.user){
 		if(!err) {
 			res.render('posts/index', {images: dbRes.rows});
+		}
+		} else {
+		res.redirect('/')
 		}
 	});
 });
@@ -107,72 +93,102 @@ app.get('/posts/new', function(req, res)	{
 		res.redirect('/')
 	}
 });
+
 // Show Posts
 app.get('/posts/:id', function(req, res)	{
 	var user = req.user;
 	db.query('SELECT * FROM images WHERE id = $1', [req.params.id], function(err, dbRes)	{
-		if(!err) {
-			res.render('posts/show', {user: user, images: dbRes.rows[0]});
-		}
-	});
-});
-
-// List of Posts
-app.post('/posts', function(req, res)	{
-	var params	=	[req.body.imagesource, req.body.location, req.body.title]
-	db.query('INSERT INTO images (imagesource, location, title) VALUES ($1, $2, $3)', params, function(err, dbRes){
-		if (!err){
-			res.redirect('/posts');
-		}
-	});
-});
-//Wrong
-// New Profile
-app.get('/users/signup', function(req, res) {
-	console.log(req.user)
-	var user = req.user
-	res.render('users/signup');
-});
-// Logout-not restful routing
-app.get('/logout', function(req, res) {
-	req.logout();
-	res.redirect('/')
-});
-// Login
-app.post('/login',
-  passport.authenticate('local', { 
-  	successRedirect: '/',
-    failureRedirect: '/login'
-  })
-);
-// New User Login
-app.post('users/signup', function(req, res){
-    db.query("UPDATE users SET name = $1, password = $2 WHERE id = $3", [req.body.name, req.body.password, req.params.id], function(err, dbRes){
-        if(err){
-            res.redirect('users/signup');
-            alert("User Not Signed up!")
-          }
-     });
-});       
-// Edit people in table
-app.get('/posts/:id/edit', function(req, res)	{
-	db.query("SELECT * FROM images WHERE id = $1", [req.params.id], function(err, dbRes){
 		if(req.user){
-		if (!err) {
-			res.render('posts/edit', { images: dbRes.rows[0]});} 
+			if(!err) {
+				res.render('posts/show', {user: user, images: dbRes.rows[0]});
+			}
 		} else {
 		res.redirect('/')
 		}
 	});
 });
-// Set edited names to new value
-app.patch('/posts/:id', function(req, res)	{
-	db.query("UPDATE images SET imagesource = $1, location = $2, title = $3 WHERE id = $4", [req.body.imagesource, req.body.location, req.body.title, req.params.id], function(err, dbRes)	{
-		if (!err)	{
-			res.redirect('/' + req.params.id);
+
+// Edit people in table
+app.get('/posts/:id/edit', function(req, res)	{
+	db.query("SELECT * FROM images WHERE id = $1", [req.params.id], function(err, dbRes){
+		if(req.user){
+			if (!err) {
+			res.render('posts/edit', { images: dbRes.rows[0]});
+			} 
+		} else {
+		res.redirect('/')
 		}
 	});
 });
+
+// New User
+app.get('/users/signup', function(req, res) {
+	var user = req.user
+	res.render('users/signup');
+});
+//Show All Images----not working
+app.get('posts/showall', function(req, res) {
+	if(req.user){
+	res.render('posts/showall', { images: dbRes.rows[0]})
+	} else {
+		res.redirect('/')
+	}
+});
+
+app.get('/test', function(req, res) {
+  db.query('SELECT * FROM images;', function(err, dbRes) {
+	  if(req.user){
+		  if(!err) {
+		  	console.log(dbRes.rows)
+		    res.render('posts/test', {images: dbRes.rows});
+			}
+		} else {
+			res.redirect('/');
+		}
+	});
+});
+	
+
+////////////////////////////////////////////////////////////
+// app.post Routes
+//New Images
+app.post('/posts', function(req, res)	{
+	var params	=	[req.body.imagesource, req.body.location, req.body.title,req.user.id]
+	db.query('INSERT INTO images (imagesource, location, title, users_id) VALUES ($1, $2, $3, $4)', params, function(err, dbRes){
+		if (!err){
+			res.redirect('/posts');
+		}
+	});
+});
+
+// Login
+app.post('/users/login',
+  passport.authenticate('local', { 
+  	successRedirect: '/',
+    failureRedirect: 'users/login'
+  })
+);
+
+// New User Login
+app.post('users/signup', function(req, res){
+	var params	=	[req.body.name, req.body.password]
+    db.query("INSERT INTO users (name, password) VALUES ($1, $2)", params, function(err, dbRes){
+        if(!err){
+            res.redirect('/');
+          } else {
+          	res.redirect('users/signup')          	
+          }
+     });
+});    
+
+////////////////////////////////////////////////////////////
+// app.delete Routes
+// Logout
+app.delete('/logout', function(req, res) {
+	req.logout();
+	res.redirect('/')
+});
+
 // Delete from table
 app.delete('/posts/:id', function(req, res)	{
 	db.query("DELETE FROM images WHERE id = $1", [req.params.id], function(err,dbRes)	{
@@ -182,7 +198,19 @@ app.delete('/posts/:id', function(req, res)	{
 	})
 });
 
-//Listen on port 3000
-app.listen(4000);
+////////////////////////////////////////////////////////////
+//app.patch Routes
+// Set edited names to new value
+app.patch('/posts/:id', function(req, res)	{
+	db.query("UPDATE images SET imagesource = $1, location = $2, title = $3, WHERE id = $4", [req.body.imagesource, req.body.location, req.body.title, req.params.id], function(err, dbRes)	{
+		if (!err)	{
+			res.redirect('posts/' + req.params.id);
+		}
+	});
+});
+
+
+//Listen on port 4000
+app.listen(process.env.PORT || 4000);
 //Inform about setup
 console.log('Server is running!')
