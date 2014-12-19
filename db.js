905ce9f2f314b8e3520de0aedc626535c1816fb3
Nic Boutte <nicboutte@gmail.com>
2014-12-19 12:51:05 -0800

    Fix recent

--- db.js
+++ db.js
@@ -8,14 +8,25 @@ var db = {};
 // host: "localhost"
 // };
 // db.connect = function(runAfterConnecting) {
-// pg.connect(db.config, function(err, client, done){
-// if (err) {
-// console.error("OOOPS!!! SOMETHING WENT WRONG!", err);
-// }
-// runAfterConnecting(client);
-// done();
-// });
+//   pg.connect(db.config, function(err, client, done){
+//     if (err) {
+//       console.error("OOOPS!!! SOMETHING WENT WRONG!", err);
+//     }
+//     runAfterConnecting(client);
+//     done();
+//   });
 // };
+db.connect = function(runAfterConnecting) {
+  console.log(process.env.DATABASE_URL);
+
+  pg.connect(process.env.DATABASE_URL, function(err, client, done){
+    if (err) {
+      console.error("OOOPS!!! SOMETHING WENT WRONG!", err);
+    }
+    runAfterConnecting(client);
+    done();
+  });
+};
 db.query = function(statement, params, callback){
 db.connect(function(client){
 client.query(statement, params, callback);
